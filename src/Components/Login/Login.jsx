cd8a16408e9c2ed4f33c017326b58f24fbe7070e
Rose-616 <raniyahnoor9988@gmail.com>
2024-08-17 13:39:55 +0500

    cookies added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 import { PropagateLoader } from 'react-spinners';
-import Cookies from 'js-cookie';
+
 
 function Login({ setFormAction, setShowAdditionalFields, setIsLoading, navigate }) {
   const [isLoading, setIsLoadingLocal] = useState(false);
