d69382bf5b29bc98018ae2784cb51c9351773211
Rose-616 <raniyahnoor9988@gmail.com>
2024-08-20 16:12:37 +0500

    cookies correctede

--- src/Components/Admin/Home/AdminHome.jsx
+++ src/Components/Admin/Home/AdminHome.jsx
@@ -28,11 +28,7 @@ const AdminHome = () => {
 
 
 
-   const getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  };
+  
 
   useEffect(() => {
     const fetchTotalRegistrations = async () => {
